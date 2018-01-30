f47fc81afcaa5e856efffd611578b6ddb25caaba
Stefan Bauer <stefan.bauer@n8d.at>
2018-01-30 16:42:22 +0100

    Responsive Web Parts added

---
+++ src/webparts/responsiveHelloWorld/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IResponsiveHelloWorldWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ResponsiveHelloWorldWebPartStrings' {
+  const strings: IResponsiveHelloWorldWebPartStrings;
+  export = strings;
+}
